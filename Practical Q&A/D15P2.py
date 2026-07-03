f841e73afc263d91f7011da28ec4bd15a2f00871
Hasna <hasnaaabasheer@gmail.com>
2026-07-03 13:18:59 +0530

    p2

--- Practical Q&A/D15P2.py
+++ Practical Q&A/D15P2.py
@@ -1,4 +1,4 @@
 def introduce(name,age,city):
-    print(f"iam {name}.iam {age} years old and iam from {city}")
+    print(f"iam {name}.iam {age} years   old and iam from {city}")
 
 introduce(city="chelari",name="hasna",age=18)
