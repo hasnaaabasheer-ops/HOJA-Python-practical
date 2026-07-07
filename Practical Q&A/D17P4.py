9b2a37238762bee298a96413993fc6869d12585d
Hasna <hasnaaabasheer@gmail.com>
2026-07-07 13:06:38 +0530

    poem2

---
+++ Practical Q&A/D17P4.py
@@ -0,0 +1,11 @@
+file = open("poem2.txt","w")
+file.write("twingle twingle little star\n")
+file.write("how i wonder what you are\n ")
+file.write("up above the world so high\n ")
+file.write("like a diamond in the sky\n ")
+file.close()
+
+file = open("poem2.txt","r")
+first_line=file.readline()
+print(first_line.split())
+file.close()
