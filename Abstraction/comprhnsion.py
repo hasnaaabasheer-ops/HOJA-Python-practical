23571ef9ddaeeaa3709d694eb18167834ccf8a85
Hasna <hasnaaabasheer@gmail.com>
2026-07-18 20:20:28 +0530

    comprehension exambles rehersing

--- Abstraction/comprhnsion.py
+++
@@ -1,3 +0,0 @@
-numbers = [1,2,4,4,5,3]
-unique_squares = {x**2 for x in numbers}
-print(unique_squares)
---
+++ Rehearsing/comprhnsn6.py
@@ -0,0 +1,3 @@
+numbers =[1,2,3,4,2,3,5]
+unique_squares ={x**2 for x in numbers}
+print(unique_squares)
